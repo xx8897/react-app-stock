704b6f96ff11f4d33228d5c1264054d7cd081761
ZhiXiang <pp21879136@gmail.com>
2021-06-23 15:37:10 +0800

    sss

--- src/components/pages/stock.js
+++ src/components/pages/stock.js
@@ -12,6 +12,7 @@ import mediatek from '../intro/mediatek'
 import npc from '../intro/npc'
 import tsmc from '../intro/tsmc'
 import umc from '../intro/umc'
+import one from './one'
 
 function Cards() {
   return (
@@ -24,13 +25,9 @@ function Cards() {
               src='images/tsmc.jpg'
               text='2330 台積電'
               label='stock'
-              path='/tsmc'
+              path='/one'
             />
-            <Router>
-              <Switch>
-                <Route path='/tsmc' component={tsmc} />
-                </Switch>
-            </Router>
+            
 
             <CardItem
               src='images/mediatek.jpg'
@@ -38,54 +35,66 @@ function Cards() {
               label='stock'
               path='/mediatek'
             />
+            
+          
             <CardItem
               src='images/honhai.png'
               text='2317 鴻海'
               label='stock'
               path='/honhai'
             />
+            
+            
             <CardItem
             src='images/delta.png'
             text='2308 台達電'
             label='stock'
             path='/delta'
             />
+
+            
             <CardItem
             src='images/umc.jpg'
             text='2303 聯電'
             label='stock'
             path='/umc'
             />
+            
             <CardItem
             src='images/fubon.png'
             text='2881 富邦金'
             label='stock'
             path='/fubon'
             />
+            
             <CardItem
             src='images/fpg.png'
             text='1301 台塑'
             label='stock'
             path='/fpg'
             />
+            
             <CardItem
             src='images/npc.jpg'
             text='1303 南亞'
             label='stock'
             path='/npc'
             />
+            
             <CardItem
             src='images/csc.jpg'
             text='2002 中鋼'
             label='stock'
             path='/csc'
             />
+            
             <CardItem
             src='images/cathaybk.png'
             text='2882 國泰金'
             label='stock'
             path='/cathaybk'
             />
+            
           </ul>
         </div>
       </div>
